017a0497330e993764ff6670d9b35953540b470d
Carlos Jug <carlos.jug@maus.mx>
2024-07-03 17:20:52 -0600

    backup server

---
+++ indax.php
@@ -0,0 +1,107 @@
+<?php 
+/*if( strstr($_SERVER['HTTP_HOST'],'www.') )
+{
+	header('Location: http://'.substr($_SERVER['HTTP_HOST'],4).$_SERVER['REQUEST_URI']);
+	exit();
+}*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Jornadas Médicas en Guadalajara">
+<meta name="keywords" content="Jornadas Médicas,Hospital San Javier,20 aniversario,Oncología, Radiocirugía, Neurocirugía ">
+<meta name="author" content="Hospital San Javier">
+<meta name="globalsign-domain-verification" content="O9hxh6UWT8LOh1DLdPs63e09J_FJUpSkvD-RRwSQJF" />
+<title>Jornadas Médicas de Aniversario Hospital San Javier</title>
+<link href="style_neuro.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="http://www.jornadasmedicassanjavier.com/hsj.ico" type="image/x-icon" />
+<script type="text/javascript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+</head>
+
+<body onload="MM_preloadImages('img/bt_home_f2.gif','img/bt_contact_f2.gif','img/bt_sponsors_f2.gif','img/bt_ifne_f2.gif','img/mn_accommodation_f2.jpg','img/mn_social_f2.jpg','img/mn_previous_f2.jpg','img/bt_send_abs_f2.jpg')">
+
+<?php include("includes/barra_fechador.php");?>
+<?php include("includes/encabezado.php");?>
+
+<?php include("includes/menu.php");?>
+
+<!-- div sombra -->
+	<div id="body_sombra"></div>
+<!-- /div sombra -->
+<div id="body_2">
+	<div id="cont_body_left">
+    	    	<div id="li_cont">
+        	<div id="tt_cont">CANCELACIÓN TEMPORAL</div>
+            <div id="tx_cont_nav"><a href="./">Inicio</a> / Jornadas de Aniversario Hospital San Javier</div>
+        </div>
+                <div id="cont_home_tx">
+                  <p> <strong>CANCELACIÓN TEMPORAL DE LAS  JORNADAS ONCOLOGÍA AL LÍMITE VII</strong> </p>
+                  <p>Apreciados amigos/as,<u></u><u></u></p>
+                  <p>Nos ponemos en contacto con ustedes para comunicarles que por las medidas que regirán el progresivo desconfinamiento en México en los próximos meses, el Comité Organizador de nuestras Jornadas <strong>Oncología al Límite VII</strong> ha decidido cancelar este año la celebración de nuestro tradicional evento previstos para los días del 26 al 28 de noviembre de 2020.<u></u><u></u></p>
+                  <p>Los mantendremos informados sobre la reprogramación de las Jornadas en el 2021.<u></u><u></u></p>
+                  <p>Lamentamos sinceramente las molestias que esta cancelación pueda causarles, agradecemos su tiempo y esfuerzo dedicado a las Jornadas, deseando que ustedes y sus seres queridos estén bien.</p>
+                  <p><br />
+                  Atentamente,</p>
+<p><br />
+                  <strong>Comité Organizador.</strong></p>
+          <div id="firma_cont">
+            <div id="firma_tx"><strong> Dr. Juan José Salazar del Valle</strong><br/> 
+            Jefatura de Capacitación, Enseñanza e Investigación GESJ</div>
+            <div id="firma_tx"><strong> Dr. Gilberto Morgan Villela</strong><br/> 
+            Oncólogo Médico,Hospital San Javier</div>
+                        <div id="firma_tx"><strong> Dr. Oscar Armando Melhado Orellana</strong><br/>
+                        Cirujano Oncólogo, Unidad de Oncología, Hospital San Javier</div>
+              <div id="firma_tx"><strong>Dr. Jorge Jiménez Tornero</strong><br/>
+		      Director de Clinica de Dolor y Paliativos del Hospital San Javier</div>
+			  <div id="firma_tx"><strong>Enf. Esp. Ana Luisa Rosas Pérez</strong><br/>
+	        Jefe de Enfermería de la Unidad de Oncología del Hospital San Javier</div>
+          </div>
+					
+          
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+                  <p >&nbsp;</p>
+<!--<p ><a href="pdf/HSJ_oncologia_al_limite_IV_2.pdf" class="td_prog_stt" download="Jornadas 20 Aniversario Hospital San Javier">Descargar versión PDF Programa general</a></p>-->
+                  <p >&nbsp;</p>
+      </div>
+    </div>
+    	<?php include("includes/banner_sj.html");?>
+
+
+
+</div>
+
+
+<?php include("includes/pie.html");?>
+
+</body>
+</html>
